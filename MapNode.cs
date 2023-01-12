59cdc43759ccb63ae6b5946237be0bbb0183523a
“Salil48” <salilchharia@gmail.com>
2023-01-12 21:05:38 +0530

    Frequency of Paragraph

--- MapNode.cs
+++ MapNode.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace HashTableandBST
 {
     //Value type Data type KeyValue
+    //Value type Data Type KeyValue
     public struct KeyValue<K, V>
     {
         public K Key { get; set; }
@@ -17,12 +18,14 @@ namespace HashTableandBST
     {
         int size;
         public LinkedList<KeyValue<K, V>>[] items;
+
         public MapNode(int size)
         {
             this.size = size;
             this.items = new LinkedList<KeyValue<K, V>>[size];
-
         }
+
+
 
         public void Add(K key, V value)
         {
@@ -96,5 +99,6 @@ namespace HashTableandBST
 
             }
         }
+
     }
 }
